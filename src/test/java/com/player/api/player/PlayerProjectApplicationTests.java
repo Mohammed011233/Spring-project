b182299876840cb650f0fa28ba521fd0360fca24
Mohamed Ali Nossir <71195812+Mohammed011233@users.noreply.github.com>
2022-11-17 10:37:14 +0200

    project init

---
+++ src/test/java/com/player/api/player/PlayerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.player.api.player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
